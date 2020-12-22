b55f3a2c6a819cba07e86014b3b259d3cc701a27
Eric <el.mimic@hotmail.com>
2020-12-22 11:58:57 +0800

    Added Form History page and enabled the search function with pagination

--- src/main/java/sg/edu/iss/ca/service/UsageFormService.java
+++ src/main/java/sg/edu/iss/ca/service/UsageFormService.java
@@ -2,6 +2,8 @@ package sg.edu.iss.ca.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+
 import sg.edu.iss.ca.model.FormCart;
 import sg.edu.iss.ca.model.UsageForm;
 
@@ -9,5 +11,6 @@ public interface UsageFormService {
 	public List<FormCart> listAllItems(UsageForm usageForm);
 	public void createForm();
 	public List<UsageForm> findUsageFormsByInventoryId(int iid);
-	
+	public Page<UsageForm> findPaginatedSearch(int pageNo, int pageSize, String keyword);
+	public Page<UsageForm> findPaginated(int pageNo, int pageSize);
 }
